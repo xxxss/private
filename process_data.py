5d4a7fa15412ceaec71eeb6800ff200f85523b3a
xxxss <xxxss@msn.com>
2016-06-07 23:16:12 +0800

    change to process all order files

--- process_data.py
+++ process_data.py
@@ -1,29 +1,34 @@
 import datetime
+import os
 
 def process():
-	f = open("D:/didi/citydata/season_1/training_data/order_data/order_data_2016-01-01")
-	req = {}
-	ans = {}
+    for root, dirs, files in os.walk('season_1/training_data/order_data'):
+        for filename in files:
+            if not filename.startswith('.') and filename.startswith('order_data'):
+                f = open(os.path.join(root, filename))
+                req = {}
+                ans = {}
 
-	for line in f:
-		orderid, driverid, userid, start, end, price, timer = line.strip().split('\t')
-		pertimer = timer_sec(timer)
-		if req.has_key((start, pertimer)):
-			req[(start,pertimer)] += 1
-		else:
-			req[(start,pertimer)] = 1
+                for line in f:
+                    orderid, driverid, userid, start, end, price, timer = line.strip().split('\t')
+                    pertimer = timer_sec(timer)
+                    if req.has_key((start, pertimer)):
+                        req[(start,pertimer)] += 1
+                    else:
+                        req[(start,pertimer)] = 1
 
-		if driverid != 'NULL':
-			if ans.has_key((start, pertimer)):
-				ans[(start,pertimer)] += 1
-			else:
-				ans[(start,pertimer)] = 1
+                    if driverid != 'NULL':
+                        if ans.has_key((start, pertimer)):
+                            ans[(start,pertimer)] += 1
+                        else:
+                            ans[(start,pertimer)] = 1
 
-	fo = open("D:/didi/citydata/season_1/request_inform","w")
-	for i, j in req.iteritems():
-		if not ans.has_key(i):
-			ans[i] = 0
-		fo.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(i[0], i[1], j, ans[i],j-ans[i]))
+                fo = open(os.path.join(root, "proceed_" + filename), "w")
+                for i, j in req.iteritems():
+                    if not ans.has_key(i):
+                        ans[i] = 0
+                    fo.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(i[0], i[1], j, ans[i],j-ans[i]))
+            
 
 
 def timer_sec(timer):
@@ -34,12 +39,15 @@ def timer_sec(timer):
 
 
 def same_point(district):
-	f = open("D:/didi/citydata/season_1/request_inform")
-	ad=[]
-	for line in f:
-		address, timerper, request, answer, gap = line.strip().split("\t")
-		if address == district:
-			ad.append((timerper, request))
-	ff = open("D:/didi/citydata/season_1/" + address,"w")
-	for i in ad:
-		ff.write("{0}\t{1}\n".format(i[0],i[1]))
+    for root, dirs, files in os.walk('season_1/training_data/order_data'):
+        for filename in files:
+            if filename.startswith('proceed_'):
+                f = open(os.path.join("season_1/training_data/order_data", filename))
+                ad=[]
+                for line in f:
+                    address, timerper, request, answer, gap = line.strip().split("\t")
+                    if address == district:
+                        ad.append((timerper, request))
+                ff = open(os.path.join("season_1/training_data/order_data", address),"w")
+                for i in ad:
+                    ff.write("{0}\t{1}\n".format(i[0],i[1]))
